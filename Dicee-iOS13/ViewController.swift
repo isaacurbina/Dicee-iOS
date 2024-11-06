ef5b93c2a898c0f8bbb603b5d8fed94fd9ad1806
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-06 17:44:11 -0600

    37. Lets Link Our Design to Our Code

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+	@IBOutlet weak var diceImageView1: UIImageView!
+	
+	@IBOutlet weak var diceImageView2: UIImageView!
+	
+	@IBOutlet weak var rollButton: UIButton!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+		diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+		diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
