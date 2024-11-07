bd8c65458923204e9194ee4d18b4dca2ae509352
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-07 17:10:00 -0600

    41. Storing Data using Variables and Arrays (#4)

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,16 +13,23 @@ class ViewController: UIViewController {
 	
 	@IBOutlet weak var diceImageView2: UIImageView!
 	
+	// region variables
+	let diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+	var leftDiceNumber = 0
+	var rightDiceNumber = 5
+	// endregion
+	
 	override func viewDidLoad() {
         super.viewDidLoad()
-		diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-		diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+		diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
+		diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
     }
 
 	@IBAction func rollButtonPressed(_ sender: Any) {
-		print("Button got tapped.")
-		diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-		diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+		leftDiceNumber = (leftDiceNumber + 1) % 6
+		rightDiceNumber = (6 - leftDiceNumber) % 6
+		diceImageView1.image = diceImages[leftDiceNumber]
+		diceImageView2.image = diceImages[rightDiceNumber]
 	}
 }
 
